fec362a9b3e97e952cc146a770a1d1627f153714
Princeberg <princebergborja@gmail.com>
2026-08-14 15:09:23 +0300

    14.08.26 01

--- js/product.js
+++ js/product.js
@@ -1,8 +1,10 @@
 const productsData = [
+    // --- PASTELS ---
     {
         id: "prod-1",
         name: "Mega Pastels",
         price: 2500,
+        category: "pastels", // Doit correspondre à l'ID de la catégorie
         desc: "3 gros pastels + poulet/Sardine + Œuf + 1pot mayo + 1pot ketchup + 1pot sauce ELENGI",
         image: "images/food.png"
     },
@@ -10,13 +12,17 @@ const productsData = [
         id: "prod-2",
         name: "Mega Pastels ELENGI",
         price: 3000,
+        category: "pastels",
         desc: "3 gros pastels + poulet/Sardine + Œuf + Fromage (Mozzarella/Cheddar) + sauces",
         image: "images/food.png"
     },
+
+    // --- BOX ---
     {
         id: "prod-3",
         name: "Bucket Pastels",
         price: 6500,
+        category: "box",
         desc: "9 Mega pastels au choix + 3 pots mayo + 3 pots ketchups + 3 pots sauce ELENGI",
         image: "images/food.png"
     },
@@ -24,7 +30,28 @@ const productsData = [
         id: "prod-4",
         name: "Bucket Pastels ELENGI",
         price: 7000,
+        category: "box",
         desc: "9 Mega pastels au choix + fromage au choix + sauces",
         image: "images/food.png"
+    },
+
+    // --- DESSERTS ---
+    {
+        id: "prod-5",
+        name: "Fondant au chocolat",
+        price: 1500,
+        category: "desserts",
+        desc: "Cœur coulant au chocolat noir",
+        image: "images/dessert.png"
+    },
+
+    // --- BOISSONS ---
+    {
+        id: "prod-6",
+        name: "Jus de Bissap",
+        price: 1000,
+        category: "boissons",
+        desc: "Jus de bissap frais fait maison (50cl)",
+        image: "images/boisson.png"
     }
 ];
